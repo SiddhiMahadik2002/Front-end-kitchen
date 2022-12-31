a2bdc0b659033dcac9a1d7b075e5a324be97eef8
Siddhi <siddhimahadik257@gmail.com>
2022-12-31 23:41:34 +0530

    fix:contact pages links

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -10,11 +10,11 @@ export const Contact=()=>{
                 <div className="socialmedia">
                     <div className="email">
                     <ion-icon name="mail-outline"></ion-icon>
-                    <p><a href="mailto:siddhimahadik257@gmail.com">siddhimahadik257@gmail.com</a></p>
+                    <p><a target="_blank" href="mailto:siddhimahadik257@gmail.com">siddhimahadik257@gmail.com</a></p>
                     </div>
                     <div className="instagram">
                     <ion-icon name="logo-instagram"></ion-icon>
-                    <p><a href="https://instagram.com/siiddhi._25">Siddhi Mahadik</a></p>
+                    <p><a target="_blank" href="https://instagram.com/siiddhi._25">Siddhi Mahadik</a></p>
                     </div>
                 </div>
             </div>
