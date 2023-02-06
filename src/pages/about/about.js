298d53de36265775f495418ef16ca2a60a6dfa43
Siddhi <siddhimahadik257@gmail.com>
2023-02-06 20:55:44 +0530

    Created about page

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -5,12 +5,17 @@ export const About= () =>{
     return(
          <div className="about">
             <div className="aboutHeader">
-                <div className="aboutoverlay">
-                    
-                </div>
-                <h1>About us</h1>
+              <div className="left">
+                <img src="https://images.pexels.com/photos/2454533/pexels-photo-2454533.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+              </div>
+              <div className="right">
+                <h2>About me</h2>
+                <p>I am Siddhi Mahadik from Chiplun, currently pursuing BE computer science from Gharda Institute of Technology. This is my one of the project, and have currently started writing food blogs for this project!</p>
+              </div>
+                
+
             </div>
-           
+            
          </div>
 
     )
